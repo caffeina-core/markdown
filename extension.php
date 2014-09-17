5280721ca9009549d25886ece26df13c401140c2
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2014-09-17 19:46:59 +0200

    Use ParsedownExtra

--- extension.php
+++ extension.php
@@ -3,7 +3,7 @@
 String::extend([
   'markdown' => function($text){
     static $markdown_parser = null;
-    if (null === $markdown_parser) $markdown_parser = new Parsedown();
+    if (null === $markdown_parser) $markdown_parser = new ParsedownExtra();
     return $markdown_parser->text($text);
   }
 ]);
